744aa17daefc773d890a1fba9aa96ddbeaf07ae9
Hvbris <44342309+hvbr1s@users.noreply.github.com>
2023-02-25 22:59:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,39 +1,51 @@
+import { createRequire } from "module"
+const require = createRequire(import.meta.url)
 const express = require("express");
-const {await} = require("axios");
 const { Configuration, OpenAIApi } = require("openai");
-const OPENAI_API_KEY = 'yourAPIKey'
+const OPENAI_API_KEY =  'your_key'
 const configuration = new Configuration({
   apiKey: OPENAI_API_KEY,
  });
 const PORT = 4088
 
+//init chat
 const openai = new OpenAIApi(configuration);
 const app = express();
 
+// middleware
 app.use(express.json()) 
-
-app.get('/', (req, res) => {
-  console.log('Hello, you have reached the home route!')
-});
-
-app.post('/', (req,res) =>{
-  res.status(200).send("'Hey what would you like to post?'")
-
-});
+app.use(express.static('public'))
 
 
+app.post("/gpt", async (req, res)=> {
+
+  const { question } = req.body
+  console.log(question)
+  let chatResponse = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: question,
+    n: 1,
+    max_tokens: 100,
+    temperature: 1,
+  })
+  const answer = chatResponse.data.choices[0].text.trim()
+  res.status(200).send({ "response": answer })
+  console.log(answer)
+})
+
+
+// test server
 app.post("/api", async (req, res) => {
   try {
 
     const {question} = req.body;
-    const userprompt = JSON.stringify(question)
     console.log(question)
 
     async function answer(){
 
       const response = await openai.createCompletion({
             model: "text-davinci-003",
-            prompt: userprompt,
+            prompt: question,
             n: 1,
             max_tokens: 50,
             temperature: 1,
@@ -43,8 +55,9 @@ app.post("/api", async (req, res) => {
     answer()
     .then((result) => {
       const answer = result.data.choices[0].text.trim()
+      console.log(answer)
       res.status(200).json(answer);
-        }) 
+      })
   } 
   
   catch (err) {
@@ -55,6 +68,5 @@ app.post("/api", async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port:${PORT}`);
-  console.log('Server is ready to receive requests.')
+  console.log(`Server running on port ${PORT} and ready to receive requests.`);
 });
